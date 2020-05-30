998bd293195cb3179b637f0f0f290b9b3afe623b
Burak <burak.kiran95@gmail.com>
2020-05-30 09:51:27 -0700

    added retry and multiple song entry

--- lyracist.py
+++ lyracist.py
@@ -2,12 +2,24 @@ import azapi
 import string
 import re
 import json
+import os
+import uuid
 from elasticsearch import Elasticsearch
 
-def getLyricsAsArray(api, songData):
-    lyrics = api.getLyrics(artist=songData['artist'], title=songData['title'], save=False)
-    #print(lyrics)
-    return lyrics.splitlines()
+def getLyricsAsArray(api, songData, retries):
+    counter = 0
+    while (counter < retries):
+        print("------------------------------")
+        print("Attempting to fetch: " + songData['title'])
+        print("------------------------------")
+        try:
+            lyrics = api.getLyrics(artist=songData['artist'], title=songData['title'], save=False, search=True, sleep=10)
+            return lyrics.splitlines()
+        except:
+            print("An exception occurred")
+            counter = counter +1
+
+    
 
 def cleanLyrics(lyricArray):
     newLyricArray = []
@@ -35,16 +47,31 @@ def storeSongInEs(esApi, songData, lyricArray):
         print(res)
 
 
-if __name__ == '__main__':
-    ES = Elasticsearch([{}])
-    api = azapi.AZlyrics()
-    
-    with open('data.json') as json_file:
+def scanFile(filename, api, esApi):
+    with open(filename) as json_file:
         songsData = json.load(json_file)
         for songData in songsData['songList']:
-            #print(songData)
-            #gets the lyrics form a-z lyrics
-            lyricArray = getLyricsAsArray(api, songData)
-            #cleans the lyrics data
-            lyricArray = cleanLyrics(lyricArray)
-            storeSongInEs(ES, songData, lyricArray)
+            if (songData['fetched'] != True):
+                lyricArray = getLyricsAsArray(api, songData, 3)
+                #cleans the lyrics data
+                lyricArray = cleanLyrics(lyricArray)
+                storeSongInEs(esApi, songData, lyricArray)
+                songData['fetched'] = True
+                writeToFile(filename, songsData)
+
+def writeToFile(filename, data):
+    # create randomly named temporary file to avoid 
+    # interference with other thread/asynchronous request
+    tempfile = os.path.join(os.path.dirname(filename), str(uuid.uuid4()))
+    with open(tempfile, 'w') as f:
+        json.dump(data, f, indent=4)
+
+    # rename temporary file replacing old file
+    os.replace(tempfile, filename)
+
+
+if __name__ == '__main__':
+    ES = Elasticsearch([{'host':'localhost','port':9200}])
+    api = azapi.AZlyrics()
+    scanFile('data.json', api, ES)
+    
